ac477d1382013077007c4720ba6942d6f4ddd250
Rajesh <mattarajesh566@gmail.com>
2026-09-15 20:15:14 +0530

    Run Playwright in headless mode

--- tests.py
+++ tests.py
@@ -29,7 +29,7 @@ def run_tests(url, test_cases):
 
     with sync_playwright() as p:
 
-        browser = p.chromium.launch(headless=False)
+        browser = p.chromium.launch(headless=True)
         request_context = p.request.new_context()
 
         for test_case in test_cases:
